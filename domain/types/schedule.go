37d45d9b1468c882503ea789bb9c3a4d90823654
William Estrada <will@futuriza.me>
2023-11-15 18:39:55 -0500

    changed payload of scheduler

--- domain/types/schedule.go
+++ domain/types/schedule.go
@@ -8,11 +8,11 @@ const (
 )
 
 type CreateScheduleInput struct {
-	Id            string                 `json:"id"`
-	StartAt       int64                  `json:"startAt"`
-	EndAt         int64                  `json:"endAt"`
-	FrecuencyType FrecuencyType          `json:"frecuencyType"`
-	Payload       map[string]interface{} `json:"payload"`
+	Id            string            `json:"id"`
+	StartAt       int64             `json:"startAt"`
+	EndAt         int64             `json:"endAt"`
+	FrecuencyType FrecuencyType     `json:"frecuencyType"`
+	Payload       SendWhatsappEvent `json:"payload"`
 }
 
 type CreateScheduleParams = CreateScheduleInput
